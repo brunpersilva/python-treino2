0f7bb54c20288cb0fa7deae186f09834eecb2ef6
brunpersilva <brunpersilva@gmail.com>
2020-08-18 00:37:53 -0300

    exercicios

---
+++ ex10.py
@@ -0,0 +1,4 @@
+a= float(input('Altura da parede? '))
+l= float(input('Largura da Parede? '))
+ar= (a*l)
+print('Sera necessário {} litros de tinta'.format((ar/2)))
---
+++ ex9.py
@@ -0,0 +1,2 @@
+n = float(input('Quantos reais voce tem?'))
+print('{} reais valem {} dolares'.format((n), (n / 5.51)))
