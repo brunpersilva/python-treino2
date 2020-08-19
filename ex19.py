695a5a22ddaf7550d87c4ea3efdf60e4554d0b8b
brunpersilva <brunpersilva@gmail.com>
2020-08-18 21:08:22 -0300

    mais exercicios

--- ex19.py
+++ ex19.py
@@ -6,4 +6,11 @@ class Person:
 p1 = Person("John", 36)
 
 print(p1.name)
-print(p1.age)
+print(p1.age)from random import shuffle
+a1 = str(input('Qual o primeiro aluno?'))
+a2 = str(input('Qual o segundo?'))
+a3 = str(input("Qual o terceiro?"))
+a4 = str(input('Qual o quarto?'))
+l = [a1, a2, a3, a4]
+shuffle(l)
+print('A ordem da apresentação sera {}'.format(l))
---
+++ ex20.py
@@ -0,0 +1,4 @@
+from pygame import mixer
+mixer.init()
+mixer.music.load('ex1.mp3')
+mixer.music.play()
